1e53e0729614a3527a2676373d1d9fe5aeb884f1
Duend3Manu <duendecito3.0@gmail.com>
2025-12-26 21:56:03 -0300

    Arreglar conflicto en auth.py

--- auth.py
+++ auth.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from functools import wraps
 from flask import session, redirect, url_for
 
@@ -9,16 +8,4 @@ def solo_admin(f):
         if 'rol' not in session or session['rol'] != 'admin':
             return redirect(url_for('home'))
         return f(*args, **kwargs)
-=======
-from functools import wraps
-from flask import session, redirect, url_for
-
-def solo_admin(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        # Si no hay rol o el rol no es admin, mandar al inicio
-        if 'rol' not in session or session['rol'] != 'admin':
-            return redirect(url_for('home'))
-        return f(*args, **kwargs)
->>>>>>> 0846279f9ea5fd866a5be85c706dab7f5dde2b77
     return decorated_function
